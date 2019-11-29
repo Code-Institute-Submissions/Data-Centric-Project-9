bef69dfa9fdc9047f6812fd2ea0b3a1485d23236
Yaoma <a.o.kalugin@gmail.com>
2019-11-29 15:58:27 +0000

    Connected application to database

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
-# Creating and application instance.
+# Creating an application instance
 app = Flask(__name__)
+
+# Connecting application to database
+app.config["MONGO_DBNAME"] = "book_reviews"
+app.config["MONGO_URI"] = "mongodb+srv://Yaoma:pfc0a7aNQiF22Lek@cluster0-xbf5e.mongodb.net/book_reviews?retryWrites=true&w=majority"
+
+# Creating an database instance
+mongo = PyMongo(app)
 
 # Test
 @app.route("/")
-def test():
-    return "Test Message"
+@app.route("/get_books")
+def get_books():
+    return render_template("base.html", books=mongo.db.books.find())
 
 
 # Deploying application on a server
